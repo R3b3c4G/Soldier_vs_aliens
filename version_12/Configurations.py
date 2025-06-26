827c410b28afbb31d220bec43d0f24de7acd6450
Windows <espinarebeca8@gmail.com>
2025-06-25 22:32:45 -0600

    Se ha creado un segundo soldado. Falta adaptar al score.

---
+++ version_12/Configurations.py
@@ -0,0 +1,228 @@
+class Configurations:
+    """
+    Clase que contiene todas las configuraciones del juego.
+    """
+    # Configuraciones de la pantalla.
+    _game_title = "Soldados vs aliens"              # Título de la ventana.
+    _screen_size = (1280, 720)                      # Resolución de la pantalla (ancho, alto).
+    _fps = 30                                       # Número máximo de FPS del videojuego.
+    _game_over_screen_time = 4                      # Tiempo que se detiene el juego al perder.
+
+    # Configuraciones del soldado.
+    _soldier_size = (142, 76)                       # Escala del soldado (ancho, alto).
+    _soldier_frames_per_row = 4                     # Número de frames que contiene cada fila de la hoja de frames.
+    _soldier_frames_per_column = 2                  # Número de filas de la hoja de frames.
+    _soldier_frame_delay = 300                      # Tiempo de cada frame del personaje (en ms) para la animación del descanso.
+    _soldier_shooting_frame_delay = 34              # Tiempo de cada frame del personaje (en ms) para la animación del disparo.
+    _soldier_speed = 12.5                           # Velocidad (en píxeles) del personaje.
+
+    # Configuraciones de los disparos.
+    _shot_size = (32, 32)                           # Escala del disparo (ancho, alto).
+    _shot_frames_per_row = 4                        # Número de frames que contiene cada fila de la hoja de frames.
+    _shot_frame_delay = 100                         # Tiempo de cada frame del disparo (en ms).
+    _shot_speed = 32.5                              # Velocidad (en píxeles) del disparo.
+    _max_gunshots = 2                               # Número máximo de disparos.
+
+    # Configuraciones de los aliens.
+    _alien_size = (78, 76)                          # Escala del alien (ancho, alto).
+    _alien_frames_per_row = 4                       # Número de frames que contiene cada fila de la hoja de frames.
+    _alien_frame_delay = 180                        # Tiempo de cada frame del disparo (en ms).
+    _alien_speed_x = 4.5                            # Velocidad en X (en píxeles) del alien.
+    _alien_speed_y = 22.5                           # Velocidad en Y (en píxeles) del alien.
+    _min_aliens = 10                                # Mínimo de enemigos que aparecen en la pantalla.
+
+    # Rutas de las imágenes utilizadas.
+    _background_image_path = "../Media/background_image.jpg"
+    _soldier_sheet_path = "../Media/soldier-idle_shooting_sheet.png"
+    _shot_sheet_path = "../Media/shot-sheet.png"
+    _alien_sheets_path = ["../Media/alien2-Sheet.png",
+                          "../Media/alien4-Sheet.png",
+                          "../Media/alien5-Sheet.png"]
+    """NUEVO."""
+    _game_over_image_path = "../Media/game_over_image.jpg"
+
+
+    """ %%%%%%%     MÉTODOS DE ACCESO.    %%%%%%%%%%%%%%%%%%%%% """
+    @classmethod
+    def get_game_title(cls) -> str:
+        """
+        Getter para _game_title.
+        """
+        return cls._game_title
+
+    @classmethod
+    def get_screen_size(cls) -> tuple[int, int]:
+        """
+        Getter para _screen_size.
+        """
+        return cls._screen_size
+
+    @classmethod
+    def get_fps(cls) -> int:
+        """
+        Getter para _fps.
+        """
+        return cls._fps
+
+    """NUEVO."""
+    @classmethod
+    def get_game_over_screen_time(cls) -> int:
+        """
+        Getter para _game_over_screen_time.
+        """
+        return cls._game_over_screen_time
+
+    @classmethod
+    def get_soldier_size(cls) -> tuple[int, int]:
+        """
+        Getter para _soldier_size.
+        """
+        return cls._soldier_size
+
+    @classmethod
+    def get_soldier_frames_per_row(cls) -> int:
+        """
+        Getter para _soldier_frames_per_row.
+        """
+        return cls._soldier_frames_per_row
+
+    @classmethod
+    def get_soldier_frames_per_column(cls) -> int:
+        """
+        Getter para _soldier_frames_per_column.
+        """
+        return cls._soldier_frames_per_column
+
+    @classmethod
+    def get_soldier_frame_delay(cls) -> int:
+        """
+        Getter para _soldier_frame_delay.
+        """
+        return cls._soldier_frame_delay
+
+    @classmethod
+    def get_soldier_shooting_frame_delay(cls) -> int:
+        """
+        Getter para _soldier_shooting_frame_delay.
+        """
+        return cls._soldier_shooting_frame_delay
+
+    @classmethod
+    def get_soldier_speed(cls) -> float:
+        """
+        Getter para _soldier_speed.
+        """
+        return cls._soldier_speed
+
+    @classmethod
+    def get_shot_size(cls) -> tuple[int, int]:
+        """
+        Getter para _shot_size.
+        """
+        return cls._shot_size
+
+    @classmethod
+    def get_shot_frames_per_row(cls) -> int:
+        """
+        Getter para _soldier_frames_per_row.
+        """
+        return cls._shot_frames_per_row
+
+    @classmethod
+    def get_shot_frame_delay(cls) -> int:
+        """
+        Getter para _shot_frame_delay.
+        """
+        return cls._shot_frame_delay
+
+    @classmethod
+    def get_shot_speed(cls) -> float:
+        """
+        Getter para _soldier_speed.
+        """
+        return cls._shot_speed
+
+    @classmethod
+    def get_max_gunshots(cls) -> int:
+        """
+        Getter para _max_gunshots.
+        """
+        return cls._max_gunshots
+
+    @classmethod
+    def get_alien_size(cls) -> tuple[int, int]:
+        """
+        Getter para _alien_size.
+        """
+        return cls._alien_size
+
+    @classmethod
+    def get_alien_frames_per_row(cls) -> int:
+        """
+        Getter para _alien_frames_per_row.
+        """
+        return cls._shot_frames_per_row
+
+    @classmethod
+    def get_alien_frame_delay(cls) -> int:
+        """
+        Getter para _alien_frame_delay.
+        """
+        return cls._alien_frame_delay
+
+    @classmethod
+    def get_alien_speed_x(cls) -> float:
+        """
+        Getter para _alien_speed_x.
+        """
+        return cls._alien_speed_x
+
+    @classmethod
+    def get_alien_speed_y(cls) -> float:
+        """
+        Getter para _alien_speed_y.
+        """
+        return cls._alien_speed_y
+
+    @classmethod
+    def get_min_aliens(cls) -> int:
+        """
+        Getter para _min_aliens.
+        """
+        return cls._min_aliens
+
+    @classmethod
+    def get_background_image_path(cls) -> str:
+        """
+        Getter para _background_image_path.
+        """
+        return cls._background_image_path
+
+    @classmethod
+    def get_soldier_sheet_path(cls) -> str:
+        """
+        Getter para _soldier_sheet_path.
+        """
+        return cls._soldier_sheet_path
+
+    @classmethod
+    def get_shot_sheet_path(cls) -> str:
+        """
+        Getter para _shot_sheet_path.
+        """
+        return cls._shot_sheet_path
+
+    @classmethod
+    def get_alien_sheets_path(cls) -> list:
+        """
+        Getter para _alien_sheets_path.
+        """
+        return cls._alien_sheets_path
+
+    """NUEVO."""
+    @classmethod
+    def get_game_over_image_path(cls) -> str:
+        """
+        Getter para _background_image_path.
+        """
+        return cls._game_over_image_path
---
+++ version_12/Soldier_vs_aliens_main.py
@@ -0,0 +1,86 @@
+# Se importan los módulos necesarios.
+import pygame
+from Configurations import Configurations
+from Game_functionalities import game_events, handle_movement, check_collisions, screen_refresh, game_over_screen     # CAMBIO.
+from Media import Background
+from Soldier import Soldier
+from pygame.sprite import Group
+from Alien import Alien
+from random import randint
+
+
+def run_game() -> None:
+    """
+    Función principal del videojuego.
+    """
+    # Se inicializa el módulo de pygame y se realizan las configuraciones iniciales.
+    pygame.init()
+    screen_size = Configurations.get_screen_size()
+    screen = pygame.display.set_mode(screen_size)
+    clock = pygame.time.Clock()  # Se usa para controlar la velocidad de fotogramas (FPS).
+
+    # Se configura el título de la ventana.
+    game_title = Configurations.get_game_title()
+    pygame.display.set_caption(game_title)
+
+    # Se crea el objeto del fondo de pantalla.
+    background = Background()
+
+    # Se crea el objeto del soldado (personaje principal).
+    soldier = Soldier(screen)
+
+    # Se crea el grupo para almacenar los disparos del soldado.
+    gunshots = Group()
+
+    """NUEVO."""
+    # Se crea el segundo soldado.
+    soldier2 = Soldier(screen)
+    # Se crea el grupo para almacenar los disparos del segundo soldado.
+    gunshots2 = Group()
+
+    # Se crea el grupo para almacenar a los enemigos.
+    aliens = Group()
+
+    # Se crea la flota inicial de enemigos.
+    min_aliens = Configurations.get_min_aliens()
+    aliens_to_spawn = min_aliens + randint(0, min_aliens)
+
+    for _ in range(aliens_to_spawn):
+        alien = Alien()
+        aliens.add(alien)
+
+    # Ciclo principal del videojuego.
+    game_over = False
+    while not game_over:
+        """CAMBIO. Ahora se considera un segundo soldado y sus disparos"""
+        # Función que administra los eventos del juego.
+        game_over = game_events(soldier, gunshots, soldier2, gunshots2)
+
+        # Si el usuario ha cerrado la ventana, entonces se termina el ciclo para cerrar los recursos de pygame.
+        if game_over:
+            break
+        """CAMBIO. Ahora se considera un segundo soldado y sus disparos"""
+        # Función que administra los movimientos.
+        handle_movement(screen, soldier, gunshots, soldier2, gunshots2, aliens)
+
+        # Función que revisa las colisiones en el juego.
+        game_over = check_collisions(screen, soldier, gunshots, soldier2, gunshots2, aliens)
+        """CAMBIO. Ahora se considera un segundo soldado y sus disparos"""
+        # Función que administra los elementos de la pantalla.
+        screen_refresh(screen, clock, background, soldier, gunshots, soldier2, gunshots2, aliens)
+
+
+
+        # Si el usuario ha perdido la partida, entonces se llama a la función que muestra la pantalla
+        # del fin del juego.
+        if game_over:
+            game_over_screen(screen)
+
+    # Cierra todos los recursos del módulo pygame.
+    pygame.quit()
+
+
+
+""" %%%%%%%     CÓDIGO A NIVEL DE MÓDULO    %%%%%%%%%%%%%%%%%%%%% """
+if __name__ == "__main__":
+    run_game()
